388d325debe43f1c714a6358b3182c48a88abe41
Ajinkya988 <120636183+Ajinkya988@users.noreply.github.com>
2023-09-27 15:19:06 +0530

    Add files via upload

---
+++ Program509.java
@@ -0,0 +1,31 @@
+//File_Packer_Unpacker
+import java.util.*;
+import java.io.*;
+
+class Program509
+{
+   public static void main(String arg[]) 
+   {
+      Scanner sobj = new Scanner(System.in);
+
+      System.out.println("Enter the name of folder : ");
+      String FolderName = sobj.nextLine();
+
+      File fobj = new File(FolderName);
+
+      try
+      {
+          boolean bRet = fobj.isDirectory();
+          if(bRet == true)
+          {
+            File list[] = fobj.listFiles();
+
+            System.out.println("Number of files in that directory are : "+list.length);
+          }
+      }
+      catch(Exception iobj)  
+      {
+         System.out.println("Exception Occured : "+iobj);
+      }
+   }
+}
---
+++ Program511.java
@@ -0,0 +1,35 @@
+//File_Packer_Unpacker
+import java.util.*;
+import java.io.*;
+
+class Program511
+{
+   public static void main(String arg[]) 
+   {
+      Scanner sobj = new Scanner(System.in);
+
+      System.out.println("Enter the name of folder : ");
+      String FolderName = sobj.nextLine();
+
+      File fobj = new File(FolderName);
+      String Header = null ;
+      
+      try
+      {
+          boolean bRet = fobj.isDirectory();
+          if(bRet == true)
+          {
+            File list[] = fobj.listFiles();
+            for(int i = 0; i< list.length ; i++)
+            {
+                Header = list[i].getName() + " " + list[i].length();
+                System.out.println(Header);
+            }
+          }
+      }
+      catch(Exception iobj)  
+      {
+         System.out.println("Exception Occured : "+iobj);
+      }
+   }
+}
